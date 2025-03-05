caf6d9daee7462a408d106e1f1727fe45317ba37
sonett-i <sam@sonetti.net>
2025-03-05 21:17:19 +1030

    PP1-70: start work on enemy health

---
+++ Assets/Engine/_Scripts/Enemies/WSHealthBar.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class WSHealthBar : MonoBehaviour
+{
+    [SerializeField] GameObject HeartContainer;
+    [SerializeField] GameObject HeartPrefab;
+
+    int health;
+
+    public void RefreshHearts()
+    {
+
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
